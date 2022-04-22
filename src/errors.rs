fc94e72db566e93b35f1c7bef873abd28950f222
BarbossHack <barbossh4ck@protonmail.com>
2022-04-22 21:09:33 +0200

    clean errors

--- src/errors.rs
+++ src/errors.rs
@@ -5,26 +5,24 @@ pub enum ShmapError {
     #[error("shm_open failed, may not exists: {:?}", _0)]
     ShmOpenFailed(i32),
 
-    #[error("shm_truncate failed: {:?}", _0)]
+    #[error("shm_truncate failed: {}", _0)]
     ShmTruncatFailed(i32),
 
-    #[error("shm_unlink failed: {:?}", _0)]
+    #[error("shm_unlink failed: {}", _0)]
     ShmUnlinkFailed(i32),
 
-    #[error(
-        "InvalidShmName, should be a null-terminated string of up to NAME_MAX (i.e., 255) characters consisting of an initial slash, followed by one or more characters, none of which are slashes"
-    )]
-    InvalidShmName,
+    #[error("CStringNulError: {}", _0)]
+    CStringNulError(#[from] std::ffi::NulError),
 
-    #[error("BincodeDecodeError: {:?}", _0)]
+    #[error("BincodeDecodeError: {}", _0)]
     BincodeDecodeError(#[from] bincode::error::DecodeError),
 
-    #[error("BincodeEncodeError: {:?}", _0)]
+    #[error("BincodeEncodeError: {}", _0)]
     BincodeEncodeError(#[from] bincode::error::EncodeError),
 
-    #[error("IO Error: {:?}", _0)]
+    #[error("IO Error: {}", _0)]
     IOError(#[from] std::io::Error),
 
-    #[error("NamedLockError: {:?}", _0)]
+    #[error("NamedLockError: {}", _0)]
     NamedLockError(#[from] named_lock::Error),
 }
--- src/shm.rs
+++ src/shm.rs
@@ -11,27 +11,28 @@ pub fn shm_open_write<T: AsRef<str>>(name: T, length: usize) -> Result<i32, Shma
     let fd = shm_open(name, libc::O_RDWR | libc::O_CREAT | libc::O_TRUNC)?;
     let ret = unsafe { libc::ftruncate(fd, length as libc::off_t) };
     if ret != 0 {
-        return Err(ShmapError::ShmTruncatFailed(ret));
+        Err(ShmapError::ShmTruncatFailed(ret))
+    } else {
+        Ok(fd)
     }
-    Ok(fd)
 }
 
 fn shm_open<T: AsRef<str>>(name: T, flags: i32) -> Result<i32, ShmapError> {
-    let name = std::ffi::CString::new(name.as_ref()).map_err(|_| ShmapError::InvalidShmName)?;
-
+    let name = std::ffi::CString::new(name.as_ref())?;
     let fd = unsafe { libc::shm_open(name.as_ref().as_ptr(), flags, 0o600) };
     if fd < 0 {
-        return Err(ShmapError::ShmOpenFailed(fd));
+        Err(ShmapError::ShmOpenFailed(fd))
+    } else {
+        Ok(fd)
     }
-    Ok(fd)
 }
 
 pub fn shm_unlink<T: AsRef<str>>(name: T) -> Result<(), ShmapError> {
-    let name = std::ffi::CString::new(name.as_ref()).map_err(|_| ShmapError::InvalidShmName)?;
-
+    let name = std::ffi::CString::new(name.as_ref())?;
     let ret = unsafe { libc::shm_unlink(name.as_ref().as_ptr()) };
     if ret != 0 {
-        return Err(ShmapError::ShmUnlinkFailed(ret));
+        Err(ShmapError::ShmUnlinkFailed(ret))
+    } else {
+        Ok(())
     }
-    Ok(())
 }
