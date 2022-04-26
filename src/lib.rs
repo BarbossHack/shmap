89b75cc407b48663c8a1bde6b60af1768cd398a9
BarbossHack <barbossh4ck@protonmail.com>
2022-04-27 00:38:05 +0200

    0.2.0 api change: add Shmap struct

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,4 @@ mod shm;
 #[cfg(test)]
 mod tests;
 
-pub use map::{get, remove, set};
+pub use map::Shmap;
--- src/map.rs
+++ src/map.rs
@@ -8,6 +8,83 @@ use serde::{de::DeserializeOwned, Serialize};
 use sha2::{Digest, Sha224};
 use std::path::Path;
 
+pub struct Shmap {}
+
+impl Shmap {
+    pub fn new() -> Self {
+        Shmap {}
+    }
+
+    pub fn get<S, T>(&self, key: S) -> Result<Option<T>, ShmapError>
+    where
+        S: AsRef<str>,
+        T: DeserializeOwned,
+    {
+        let key = sanitize_key(key);
+
+        let lock = NamedLock::create(&key)?;
+        let _guard = lock.lock()?;
+
+        let mmap = match || -> Result<Mmap, ShmapError> {
+            let fd = shm_open_read(&key)?;
+            let mmap = unsafe { Mmap::map(fd) }?;
+            Ok(mmap)
+        }() {
+            Ok(mmap) => mmap,
+            Err(_) => {
+                let _ = self.remove(&key);
+                return Ok(None);
+            }
+        };
+
+        let (value, _): (T, usize) =
+            bincode::serde::decode_from_slice(mmap.as_ref(), bincode::config::standard())?;
+        Ok(Some(value))
+    }
+
+    pub fn set<S, T>(&self, key: S, value: T) -> Result<(), ShmapError>
+    where
+        S: AsRef<str>,
+        T: Serialize,
+    {
+        let key = sanitize_key(key);
+
+        let bytes = bincode::serde::encode_to_vec(&value, bincode::config::standard())?;
+
+        let lock = NamedLock::create(&key)?;
+        let _guard = lock.lock()?;
+
+        match || -> Result<(), ShmapError> {
+            let fd = shm_open_write(&key, bytes.len())?;
+            let mut mmap = unsafe { MmapMut::map_mut(fd) }?;
+            mmap.copy_from_slice(bytes.as_slice());
+            Ok(())
+        }() {
+            Ok(_) => {}
+            Err(e) => {
+                let _ = self.remove(&key);
+                return Err(e);
+            }
+        };
+
+        Ok(())
+    }
+
+    pub fn remove<S>(&self, key: S) -> Result<(), ShmapError>
+    where
+        S: AsRef<str>,
+    {
+        let key = sanitize_key(key);
+
+        let lock = NamedLock::create(&key)?;
+        let _guard = lock.lock()?;
+
+        let _ = std::fs::remove_file(Path::new("/tmp").join(format!("{}.lock", &key)));
+        shm_unlink(&key)?;
+        Ok(())
+    }
+}
+
 fn sanitize_key<S>(key: S) -> String
 where
     S: AsRef<str>,
@@ -15,73 +92,4 @@ where
     let mut hasher = Sha224::new();
     hasher.update(key.as_ref());
     format!("sham.{:x}", hasher.finalize())
-}
-
-pub fn get<S, T>(key: S) -> Result<Option<T>, ShmapError>
-where
-    S: AsRef<str>,
-    T: DeserializeOwned,
-{
-    let key = sanitize_key(key);
-
-    let lock = NamedLock::create(&key)?;
-    let _guard = lock.lock()?;
-
-    let mmap = match || -> Result<Mmap, ShmapError> {
-        let fd = shm_open_read(&key)?;
-        let mmap = unsafe { Mmap::map(fd) }?;
-        Ok(mmap)
-    }() {
-        Ok(mmap) => mmap,
-        Err(_) => {
-            let _ = remove(&key);
-            return Ok(None);
-        }
-    };
-
-    let (value, _): (T, usize) =
-        bincode::serde::decode_from_slice(mmap.as_ref(), bincode::config::standard())?;
-    Ok(Some(value))
-}
-
-pub fn set<S, T>(key: S, value: T) -> Result<(), ShmapError>
-where
-    S: AsRef<str>,
-    T: Serialize,
-{
-    let key = sanitize_key(key);
-
-    let bytes = bincode::serde::encode_to_vec(&value, bincode::config::standard())?;
-
-    let lock = NamedLock::create(&key)?;
-    let _guard = lock.lock()?;
-
-    match || -> Result<(), ShmapError> {
-        let fd = shm_open_write(&key, bytes.len())?;
-        let mut mmap = unsafe { MmapMut::map_mut(fd) }?;
-        mmap.copy_from_slice(bytes.as_slice());
-        Ok(())
-    }() {
-        Ok(_) => {}
-        Err(e) => {
-            let _ = remove(&key);
-            return Err(e);
-        }
-    };
-
-    Ok(())
-}
-
-pub fn remove<S>(key: S) -> Result<(), ShmapError>
-where
-    S: AsRef<str>,
-{
-    let key = sanitize_key(key);
-
-    let lock = NamedLock::create(&key)?;
-    let _guard = lock.lock()?;
-
-    let _ = std::fs::remove_file(Path::new("/tmp").join(format!("{}.lock", &key)));
-    shm_unlink(&key)?;
-    Ok(())
 }
